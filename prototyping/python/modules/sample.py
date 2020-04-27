630f34c5de3caba0e555d7ca87ab7406d326489b
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-04-27 17:14:32 -0500

    Add simple test for sample class

--- prototyping/python/modules/sample.py
+++ prototyping/python/modules/sample.py
@@ -2,8 +2,9 @@
 class Sample(object):
 
     #===========================================================================
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, value: float):
+        if isinstance(value, float):
+            self.value = value
 
     def __str__(self) -> str:
         return f"<Sample_obj: value = {self.value}>"
--- prototyping/python/test.py
+++ prototyping/python/test.py
@@ -93,5 +93,17 @@ class PointTest(unittest.TestCase):
         self.assertTrue(p3 > p2)
 
 #===============================================================================
+class SampleTest(unittest.TestCase):
+
+    #===========================================================================
+    def test_is_float(self):
+        s1 = sample.Sample(1)
+        s2 = sample.Sample(1.0)
+
+        self.assertFalse(hasattr(s1, "value"))
+        self.assertTrue(isinstance(s2, sample.Sample))
+        self.assertTrue(isinstance(s2.value, float))
+
+#===============================================================================
 if __name__ == "__main__":
     unittest.main()
