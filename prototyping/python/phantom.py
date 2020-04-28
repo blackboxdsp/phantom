cf74add6d0287ee5fa03eb6cfc8e84dd6d3d4a24
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-04-27 19:45:15 -0500

    Cleanup code and add more exporting for different inflection point positions

--- prototyping/python/phantom.py
+++ prototyping/python/phantom.py
@@ -13,23 +13,30 @@ def write_wav_file(frequency, rate, time):
     sinetable.fill_table()
     sinetable.set_phase_delta(frequency, rate)
 
-    inflections = [point.Point(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0))]
-    saw_phasor = phasor.Phasor(inflections)
-    
-    for i in range(output.buffer_size):
-        value = sinetable.get_next_sample(saw_phasor)
-        output.set_sample(i, value)
+    inflections = []
+    inflection_count = 3
+    for i in range(1, inflection_count + 1):
+        phase = float(i / inflection_count)
+        inflection = point.Point(phase, random.uniform(0.0, 1.0))
+        inflections.append(inflection)
 
-    output.normalize()
-    output.export_wav(f"sounds/sine-{frequency}-{inflections[0].x:.1}-{inflections[0].y:.1}.wav", rate)
+    for inflection in inflections:
+        saw_phasor = phasor.Phasor([inflection])
+    
+        for i in range(output.buffer_size):
+            value = sinetable.get_next_sample(saw_phasor)
+            output.set_sample(i, value)
+
+        output.normalize()
+        output.export_wav(f"sounds/sine-{frequency}-{inflection.x:.1}-{inflection.y:.1}.wav", rate)
 
 #===============================================================================
 def main():
     rate = 44100
-    time = 1.6
+    time = 1.2
 
     base_frequency = 55
-    num_octaves = 4
+    num_octaves = 3
     frequencies = [base_frequency * (2 ** i) for i in range(num_octaves)]
 
     for frequency in frequencies:
