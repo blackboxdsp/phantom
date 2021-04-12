be2884c3369942413f375570d36fa54e4552948a
maxwellmattryan <maxwellmattryan@gmail.com>
2021-04-12 16:33:12 -0500

    Add source files for analyzer

---
+++ src/PhantomAnalyzer.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    PhantomAnalyzer.cpp
+    Created: 12 Apr 2021 16:27:57
+    Author:  Matthew Maxwell
+
+  ==============================================================================
+*/
+
+#include "PhantomAnalyzer.h"
+
+PhantomAnalyzer::PhantomAnalyzer()
+{
+    
+}
+
+PhantomAnalyzer::~PhantomAnalyzer()
+{
+
+}
---
+++ src/PhantomAnalyzer.h
@@ -0,0 +1,25 @@
+/*
+  ==============================================================================
+
+    PhantomAnalyzer.h
+    Created: 12 Apr 2021 16:27:57
+    Author:  Matthew Maxwell
+
+  ==============================================================================
+*/
+
+#include "JuceHeader.h"
+
+/**
+ * The GUI component for displaying the frequency information of the signal.
+ */
+class PhantomAnalyzer : public Component,
+                        private Timer
+{
+public:
+    PhantomAnalyzer();
+    ~PhantomAnalyzer();
+
+private:
+
+};
