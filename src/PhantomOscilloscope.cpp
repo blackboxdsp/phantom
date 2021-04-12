531957df50be8c80f22eb9cb6bb48d3f73105ad7
maxwellmattryan <maxwellmattryan@gmail.com>
2021-04-12 18:00:40 -0500

    Add source files for oscilloscope

---
+++ src/PhantomOscilloscope.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    PhantomOscilloscope.cpp
+    Created: 12 Apr 2021 17:56:42
+    Author:  Matthew Maxwell
+
+  ==============================================================================
+*/
+
+#include "PhantomOscilloscope.h"
+
+PhantomOscilloscope::PhantomOscilloscope()
+{
+    
+}
+
+PhantomOscilloscope::~PhantomOscilloscope()
+{
+
+}
---
+++ src/PhantomOscilloscope.h
@@ -0,0 +1,25 @@
+/*
+  ==============================================================================
+
+    PhantomOscilloscope.h
+    Created: 12 Apr 2021 17:56:42
+    Author:  Matthew Maxwell
+
+  ==============================================================================
+*/
+
+#include "JuceHeader.h"
+
+/**
+ * The GUI component for displaying the amplitude information for the synthesizer
+ * output.
+ */
+class PhantomOscilloscope
+{
+public:
+    PhantomOscilloscope();
+    ~PhantomOscilloscope();
+
+private:
+
+};
