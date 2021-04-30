7995c21f61e5250d631d5c51c92679f33bd59782
maxwellmattryan <maxwellmattryan@gmail.com>
2021-04-30 17:36:18 -0500

    BUILD: Windows - Fri, Apr 30, 2021 17:36:18

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,9 @@ BUILD_TYPE=Release
 DIST_DIR=dist
 OPER_SYS=Windows
 
+PACKAGE_STEP=false
+DISTRIBUTE_STEP=false
+
 start_time=$(date +%s)
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
@@ -16,31 +19,52 @@ if [ "$BRANCH" != "dist" ]; then
     exit 1;
 fi
 
-rm -rf ./bin
+for i in "$@"; do
+    case $i in
+    -p|--package)
+        PACKAGE_STEP=true
+        shift
+        ;;
+    -d|--distribute)
+        DISTRIBUTE_STEP=true
+        shift
+        ;;
+    esac
+done
 
-scripts/build.sh -p -b=${BUILD_TYPE}
+if [ "$PACKAGE_STEP" = true ]; then
+    rm -rf ./bin
 
-if [[ ${OSTYPE} == "darwin"* ]]; then
-    OPER_SYS=MacOS
+    scripts/build.sh -p -b=${BUILD_TYPE}
 
-    rm -rf "${DIST_DIR}/macos"
-    mkdir "${DIST_DIR}/macos" 
+    if [[ ${OSTYPE} == "darwin"* ]]; then
+        OPER_SYS=MacOS
 
-    cp -r "./bin/${PLUGIN_NAME}_artefacts/VST3/${PLUGIN_NAME}.vst3" "${DIST_DIR}/macos/${PLUGIN_NAME}.vst3"
-    cp -r "./bin/${PLUGIN_NAME}_artefacts/AU/${PLUGIN_NAME}.component" "${DIST_DIR}/macos/${PLUGIN_NAME}.component"
+        rm -rf "${DIST_DIR}/macos"
+        mkdir "${DIST_DIR}/macos" 
 
-else
-    OPER_SYS=Windows
+        cp -r "./bin/${PLUGIN_NAME}_artefacts/VST3/${PLUGIN_NAME}.vst3" "${DIST_DIR}/macos/${PLUGIN_NAME}.vst3"
+        cp -r "./bin/${PLUGIN_NAME}_artefacts/AU/${PLUGIN_NAME}.component" "${DIST_DIR}/macos/${PLUGIN_NAME}.component"
 
-    rm -rf "${DIST_DIR}/windows" 
-    mkdir "${DIST_DIR}/windows"
+    else
+        OPER_SYS=Windows
 
-    cp "./bin/${PLUGIN_NAME}_artefacts/${BUILD_TYPE}/VST3/${PLUGIN_NAME}.vst3/Contents/x86_64-win/${PLUGIN_NAME}.vst3" "${DIST_DIR}/windows/${PLUGIN_NAME}.vst3"
+        rm -rf "${DIST_DIR}/windows" 
+        mkdir "${DIST_DIR}/windows"
+
+        cp "./bin/${PLUGIN_NAME}_artefacts/${BUILD_TYPE}/VST3/${PLUGIN_NAME}.vst3/Contents/x86_64-win/${PLUGIN_NAME}.vst3" "${DIST_DIR}/windows/${PLUGIN_NAME}.vst3"
+    fi
+
+    git add .
+    git commit -m "BUILD: ${OPER_SYS} - $(date)"
+    git push
+
+    echo -e "\n[Success] Package plugin for distribution!"
 fi
 
-git add .
-git commit -m "BUILD: ${OPER_SYS} - $(date)"
-git push
+if [ "$DISTRIBUTE_STEP" = true ]; then
+    echo -e "\n[TODO] Bash stuff for distributing files."
+fi
 
 convertsecs() {
     ((m = (${1} % 3600) / 60))
